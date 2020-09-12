08623f660f66ea9b30aa7e2d26f4e7e74d8bda81
shyamshr93 <shyamshr93@gmail.com>
2020-09-12 19:46:44 +0530

    channnnnnnnnnnnnnnnnnnnnnnn

--- js/post.js
+++ js/post.js
@@ -1,6 +1,14 @@
 let params = (new URL(document.location)).searchParams;
 let name = params.get("id");
 
-const title = document.getElementById('title');
+const elTitle = document.getElementById('elTitle');
+const elDate = document.getElementById('elDate');
+const elImage = document.getElementById('elPostImage');
 
-title.innerHTML = name;
+console.log("./assets/post/1.png");
+if(name == "1"){
+    elTitle.innerHTML = "Nomtes updated with many new Icon, Bookmark support, Videos support and more...";
+    elDate.innerHTML = "17 Sep 2020";
+    elImage.src = "./assets/post/1.jpg";
+    
+}
