8d247367b6e341378f301a649aa7470fda04a8d1
shyamshr93 <shyamshr93@gmail.com>
2020-09-20 17:27:06 +0530

    changes

--- js/post.js
+++ js/post.js
@@ -10,10 +10,10 @@ const elSummary = document.getElementById('elSummary');
 
 console.log("./assets/post/1.png");
 if(name == "1"){
-    elTitle.innerHTML = "Nomtes updated with many new Icon, Bookmark support, Videos support and more...";
+    elTitle.innerHTML = "Nomtes updated with new Icon, Bookmark support, Videos support and more...";
     elDate.innerHTML = "17 Sep 2020";
     elImage.src = "./assets/post/1.jpg";
-    elTopDesc.innerHTML = "In August this year we launched our first android application “NOMTES”, a all in one notes app for BCA students in college where they can easily access quality notes, books, syllabus, Question paper, lectures videos of all subjects.<br><br>\
+    elTopDesc.innerHTML = "In August this year we launched our first android application “NOMTES”, all in one notes app for BCA students which are in ipu college where they can easily access quality notes, books, syllabus, Question paper, lectures videos of all subjects.<br><br>\
     We have been working hard since then to continuously improve the app. Nomtes didn’t had all the features we planned for in the first version and therefore, we are very excited to announce the new update which includes all the features missing in the first version and more. There are new features, updated UI and more in this update. It is more beautiful, powerful and fast than before.<br><br>\
     Here’s a quick list of some of the changes which we have made in the new update.\
 ";
